ff58db4fe5c277226b54656dc9ec778afcdbd1c7
Upendra Kumar <upendra.kumar48762@gmail.com>
2021-11-16 15:20:06 +0530

    app tested

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
-from prediction_service import prediction
+#from prediction_service import prediction
+import yaml
+import joblib
 
 
+webapp_root = "webapp"
+
+params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -12,14 +17,40 @@ template_dir = os.path.join(webapp_root, "templates")
 app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predcit(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)    
+    print(prediction)
+    return prediction[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response": response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again"}
+        return error
+
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     if request.method == "POST":
         try:
             if request.form:
-                dict_req = dict(request.form)
-                response = prediction.form_response(dict_req)
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response = predcit(data)
+                #response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
             elif request.json:
                 response = prediction.api_response(request.json)
